a8fba4532d2d39a879be191fbd831ac143893e12
ulisse91 <fedo.coro@gmail.com>
2016-05-30 21:01:56 +0200

    file explorer su sdcard pare funzionare

--- app/src/main/java/ulisse/filesystemforensics/MainActivity.java
+++ app/src/main/java/ulisse/filesystemforensics/MainActivity.java
@@ -1,13 +1,76 @@
 package ulisse.filesystemforensics;
 
-import android.support.v7.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Environment;
+import android.support.annotation.MainThread;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.IOException;
 
+public class MainActivity extends Activity {
+
+    private static final int REQUEST_PATH = 1;
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 0;
+    String curFileName;
+    EditText edittext;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+       setContentView(R.layout.activity_main);
+
+        // La cosa più importante dell'universo da Marshmallow in poi a quanto pare
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+    }
+
+   /*
+   NON ho ancora capito se serve o meno
+   @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }*/
+
+    public void getfile(View view){
+        Intent intent1 = new Intent(this, FileChooser.class);
+        startActivityForResult(intent1,REQUEST_PATH);
+    }
+    // Listen for results.
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        // See which child activity is calling us back.
+        if (requestCode == REQUEST_PATH){
+            if (resultCode == RESULT_OK) {
+                curFileName = data.getStringExtra("GetFileName");
+                edittext.setText(curFileName);
+            }
+        }
     }
 }
