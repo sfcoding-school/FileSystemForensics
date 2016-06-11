7b9b147f0702ee2fe3b777e0a55d6226863022ff
ulisse91 <fedo.coro@gmail.com>
2016-06-11 17:05:00 +0200

    ancora cose

--- javascripts/myTerminal.js
+++ javascripts/myTerminal.js
@@ -338,7 +338,7 @@ function getFile(commands, term){
   pathFile = currentFolder + "/" + pathFile;
 
   console.log("pathFile " + pathFile);
-
+  term.echo("http://127.0.0.1:8001/" + global_json["SERIAL"] + "/" + commands[0] + ".zip");
   return; // da togliere ovviamente dopo
   $.ajax({
     async: false,
@@ -346,6 +346,7 @@ function getFile(commands, term){
     url: "http://127.0.0.1:8001/terminal?cmd=getfile&id=x&path=" + pathFile,
     success: function(data, status){
       console.log("callback getFile function");
+      term.echo("http://127.0.0.1:8001/" + global_json["SERIAL"] + "/" + commands[0] + ".zip");
     }
   });
 
