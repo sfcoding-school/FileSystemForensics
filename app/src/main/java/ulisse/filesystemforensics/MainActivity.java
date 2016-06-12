f9970f312a704172874ba3a6e69fe5a6c66a5d2d
matteo <matteo.capparrucci@gmail.com>
2016-06-12 18:28:14 +0200

    pulizia codice

--- app/src/main/java/ulisse/filesystemforensics/MainActivity.java
+++ app/src/main/java/ulisse/filesystemforensics/MainActivity.java
@@ -58,31 +58,4 @@ public class MainActivity extends Activity {
         //
     }
 
-   /*
-   NON ho ancora capito se serve o meno
-   @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-
-                } else {
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                }
-                return;
-            }
-
-            // other 'case' lines to check for other
-            // permissions this app might request
-        }
-    }*/
-
 }
